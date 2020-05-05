c90e4ebba581688993aceaff6ad3ac7d9df64cf0
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-05 08:32:27 +0800

    Pass arguments to query for jobs component

--- app/components/Jobs.tsx
+++ app/components/Jobs.tsx
@@ -5,8 +5,8 @@ import getJobs from "../jobs/queries/getJobs"
 import { Container, Columns } from "react-bulma-components"
 import ErrorBoundary from "app/components/ErrorBoundary"
 
-const Jobs = () => {
-  const [jobs] = useQuery(getJobs)
+const Jobs = (props) => {
+  const [jobs] = useQuery(getJobs, props.args)
 
   return (
     <Container style={{ padding: "3rem" }}>
@@ -21,11 +21,11 @@ const Jobs = () => {
   )
 }
 
-const WrappedJobs = () => {
+const WrappedJobs = (props) => {
   return (
     <ErrorBoundary fallback={(error) => <div>Error: {JSON.stringify(error)}</div>}>
       <Suspense fallback={<div>Loading...</div>}>
-        <Jobs />
+        <Jobs args={props.args} />
       </Suspense>
     </ErrorBoundary>
   )
