478e7ee674b04c3ac75b0a4d5de196ad6f467e6a
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-08 10:32:25 +0800

    Add jobs CRUD api

---
+++ app/jobs/api/createJob.ts
@@ -0,0 +1,82 @@
+import db, { JobCreateArgs, Job } from "db"
+
+interface Company {
+  id: number
+}
+
+const createJob = async (args: JobCreateArgs) => {
+  if (!args.data) return false
+  if (!args.data.company) return false
+  let jobFound: Job
+  if (args.data.aggId) {
+    const jobQuery = await db.job.findMany({
+      where: {
+        aggId: {
+          equals: `${args.data.aggId}`,
+        },
+      },
+    })
+    if (jobQuery.length) jobFound = jobQuery[0]
+  }
+  const companyQuery = await db.company.findMany({
+    where: {
+      name: {
+        equals: `${args.data.company}`,
+      },
+    },
+  })
+  let company: Company
+  if (companyQuery.length) {
+    company = companyQuery[0]
+  } else {
+    company = await db.company.create({
+      data: {
+        name: `${args.data.company}`,
+        imgUrl: `${args.data.avatar}`,
+      },
+    })
+  }
+  const jobData = {
+    ...args,
+    data: {
+      ...args.data,
+      company: {
+        connect: {
+          id: company.id,
+        },
+      },
+    },
+  }
+  if (jobFound) {
+    return await db.job.update({
+      ...jobData,
+      where: {
+        id: jobFound.id,
+      },
+    })
+  }
+  await db.job.create(jobData)
+}
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    // Process a POST request
+    if (!req.body) {
+      res.statusCode = 400
+      res.end("No body provided")
+    }
+    const job = await createJob({ data: req.body })
+    if (job) {
+      res.statusCode = 201
+      res.setHeader("Content-Type", "application/json")
+      res.end(JSON.stringify(job))
+    } else {
+      res.statusCode = 422
+      res.end("Unable to create job. Check POST body")
+    }
+  } else {
+    // Handle any other HTTP method
+    res.setHeader("Allow", ["POST"])
+    res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
