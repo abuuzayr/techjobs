71c11d90eb4f42b2386325d71283ee2c2627d43d
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-09 14:18:43 +0800

    Add tabbing functionality

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -17,102 +17,118 @@ const JobsCount = (props) => {
   return <span style={{ marginLeft: 5 }}>({jobsCount})</span>
 }
 
-const HeroComponent = () => (
-  <Hero color="info" size="medium">
-    <Hero.Head>
-      <NavBar />
-    </Hero.Head>
-    <HeroBody style={{ marginTop: -41 }}>
-      <div className="credits">
-        <Level.Side align="right">
-          <a
-            className="unsplash"
-            href="https://unsplash.com/@kp89_?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="Download free do whatever you want high-resolution photos from Kirill Petropavlov"
-          >
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                <title>unsplash-logo</title>
-                <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
-              </svg>
-            </span>
-            <span>Kirill Petropavlov</span>
-          </a>
-        </Level.Side>
-      </div>
-      <Container>
-        <Title size={1} renderAs="h1" className="has-text-centered">
-          Find a job in <Sun /> Singapore
-        </Title>
-      </Container>
-      <Container>
-        <Columns>
-          <Columns.Column size="half" offset="one-quarter">
-            <div className="field" style={{ marginTop: 15 }}>
-              <p className="control has-icons-left">
-                <input type="text" className="input is-large" placeholder="e.g. python, javascript" />
-                <span className="icon is-left">
-                  <FiSearch />
-                </span>
-              </p>
-              {/* TODO: Add quick search buttons for languages, etc. */}
-            </div>
-          </Columns.Column>
-        </Columns>
-      </Container>
-    </HeroBody>
-    <Hero.Footer style={{ marginTop: -41 }}>
-      <Tabs type="boxed" fullwidth={true}>
+const HeroComponent = (props) => {
+  const tabProps = (tab) => ({
+    tabIndex: 0,
+    role: "button",
+    onClick: () => props.setTab(tab),
+    onKeyUp: () => props.setTab(tab),
+  })
+  return (
+    <Hero color="info" size="medium">
+      <Hero.Head>
+        <NavBar />
+      </Hero.Head>
+      <HeroBody style={{ marginTop: -41 }}>
+        <div className="credits">
+          <Level.Side align="right">
+            <a
+              className="unsplash"
+              href="https://unsplash.com/@kp89_?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Download free do whatever you want high-resolution photos from Kirill Petropavlov"
+            >
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                  <title>unsplash-logo</title>
+                  <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+                </svg>
+              </span>
+              <span>Kirill Petropavlov</span>
+            </a>
+          </Level.Side>
+        </div>
         <Container>
-          <ul>
-            <li className="is-active">
-              <a>
-                Featured{" "}
-                <Suspense fallback={<></>}>
-                  <JobsCount />
-                </Suspense>
-              </a>
-            </li>
-            <li>
-              <a>Aggregated</a>
-            </li>
-            <li>
-              <a>Resources</a>
-            </li>
-          </ul>
+          <Title size={1} renderAs="h1" className="has-text-centered">
+            Find a job in <Sun /> Singapore
+          </Title>
         </Container>
-      </Tabs>
-    </Hero.Footer>
-    <style jsx>{`
-      .credits {
-        position: absolute;
-        top: 70px;
-        right: 20px;
-      }
-      .unsplash {
-        color: white;
-        padding: 4px 6px;
-        font-size: 12px;
-        line-height: 1.2;
-        border-radius: 3px;
-        opacity: 0.6;
-      }
-      .unsplash span {
-        display: inline-block;
-        padding: 2px 3px;
-      }
-      .unsplash span svg {
-        height: 12px;
-        width: auto;
-        position: relative;
-        vertical-align: middle;
-        top: -2px;
-        fill: white;
-      }
-    `}</style>
-  </Hero>
-)
+        <Container>
+          <Columns>
+            <Columns.Column size="half" offset="one-quarter">
+              <div className="field" style={{ marginTop: 15 }}>
+                <p className="control has-icons-left">
+                  <input type="text" className="input is-large" placeholder="e.g. python, javascript" />
+                  <span className="icon is-left">
+                    <FiSearch />
+                  </span>
+                </p>
+                {/* TODO: Add quick search buttons for languages, etc. */}
+              </div>
+            </Columns.Column>
+          </Columns>
+        </Container>
+      </HeroBody>
+      <Hero.Footer style={{ marginTop: -41 }}>
+        <Tabs type="boxed" fullwidth={true}>
+          <Container>
+            <ul>
+              <li className={props.tab === "featured" ? "is-active" : ""}>
+                <a {...tabProps("featured")}>
+                  Featured{" "}
+                  <Suspense fallback={<></>}>
+                    <JobsCount args={{ where: { type: { equals: "featured" } } }} />
+                  </Suspense>
+                </a>
+              </li>
+              <li className={props.tab === "aggregated" ? "is-active" : ""}>
+                <a {...tabProps("aggregated")}>
+                  Aggregated{" "}
+                  <Suspense fallback={<></>}>
+                    <JobsCount args={{ where: { type: { equals: "aggregated" } } }} />
+                  </Suspense>
+                </a>
+              </li>
+              <li className={props.tab === "liked" ? "is-active" : ""}>
+                <a {...tabProps("liked")}>Liked (0)</a>
+              </li>
+              <li className={props.tab === "resources" ? "is-active" : ""}>
+                <a {...tabProps("resources")}>Resources</a>
+              </li>
+            </ul>
+          </Container>
+        </Tabs>
+      </Hero.Footer>
+      <style jsx>{`
+        .credits {
+          position: absolute;
+          top: 70px;
+          right: 20px;
+        }
+        .unsplash {
+          color: white;
+          padding: 4px 6px;
+          font-size: 12px;
+          line-height: 1.2;
+          border-radius: 3px;
+          opacity: 0.6;
+        }
+        .unsplash span {
+          display: inline-block;
+          padding: 2px 3px;
+        }
+        .unsplash span svg {
+          height: 12px;
+          width: auto;
+          position: relative;
+          vertical-align: middle;
+          top: -2px;
+          fill: white;
+        }
+      `}</style>
+    </Hero>
+  )
+}
 
 export default HeroComponent
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react"
 import { Head, Link } from "blitz"
 import Hero from "../components/Hero"
 import Jobs from "../components/Jobs"
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>techjobs</title>
-      <link rel="icon" href="/favicon.png" />
-    </Head>
-    <Hero />
-    <Jobs />
-  </div>
-)
+const Home = () => {
+  const [tab, setTab] = useState("featured")
+  return (
+    <div>
+      <Head>
+        <title>techjobs</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Hero tab={tab} setTab={setTab} />
+      {["featured", "aggregated"].includes(tab) ? (
+        <Jobs args={{ where: { type: { equals: tab } } }} />
+      ) : (
+        <div>{tab}</div>
+      )}
+    </div>
+  )
+}
 
 export default Home
