c6d407fded31acab7260ec333239a31e93ec4d19
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-10 17:52:00 +0800

    Add create company api

---
+++ app/companies/api/createCompany.ts
@@ -0,0 +1,30 @@
+import db, { CompanyCreateArgs } from "db"
+
+async function createCompany(args: CompanyCreateArgs) {
+  const company = await db.company.create(args)
+
+  return company
+}
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    // Process only if body supplied
+    if (!req.body) {
+      res.statusCode = 400
+      res.end("No body provided")
+    }
+    const company = await createCompany({ data: req.body })
+    if (company) {
+      res.statusCode = 201
+      res.setHeader("Content-Type", "application/json")
+      res.end(JSON.stringify(company))
+    } else {
+      res.statusCode = 422
+      res.end("Unable to create company. Check POST body")
+    }
+  } else {
+    // Handle any other HTTP method
+    res.setHeader("Allow", ["POST"])
+    res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
