8a7dc6a13478f5a37c4cf93fcc228bf3f913f1d0
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-17 11:01:43 +0800

    Extract logo to its own component

---
+++ app/components/Logo.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+const Placeholder = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2em;
+  border-radius: 10px;
+  background: aliceblue;
+  font-weight: 700;
+`
+
+const Logo = ({ job }) => {
+  const { avatar, company, name } = job
+  return (
+    <figure className="image is-64x64">
+      {avatar || (company && company.imgUrl && company.imgUrl !== "https://") ? (
+        <img src={avatar || (company && company.imgUrl)} alt="Company logo" />
+      ) : (
+        <Placeholder>{company ? company.name.slice(0, 1) : name.slice(0, 1)}</Placeholder>
+      )}
+    </figure>
+  )
+}
+
+export default Logo
