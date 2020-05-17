dbaec2d7c6d97cc69924552152ec0103c2485898
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-17 09:36:14 +0800

    Add fix for local dev prisma client

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,14 @@
 import { PrismaClient } from "@prisma/client"
 export * from "@prisma/client"
 
-const prisma = new PrismaClient()
+let prisma
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient()
+} else {
+  // Ensure the prisma instance is re-used during hot-reloading
+  global.prisma = global.prisma || new PrismaClient()
+  prisma = global.prisma
+}
 
 export default prisma
