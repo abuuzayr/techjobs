51fb5852bdade7acf5600e1a805e5f4ec72178bf
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-20 00:48:54 +0800

    Move apply component to standalone

---
+++ app/components/Apply.tsx
@@ -0,0 +1,18 @@
+import { Button } from "react-bulma-components"
+
+const Apply = ({ url }) => (
+  <Button
+    renderAs="a"
+    color="info"
+    outlined={true}
+    rounded={true}
+    style={{ borderWidth: 2, fontWeight: 600 }}
+    href={url}
+    target="_blank"
+    onClick={(e) => e.stopPropagation()}
+  >
+    Apply
+  </Button>
+)
+
+export default Apply
