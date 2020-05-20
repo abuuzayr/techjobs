57f7cb388f186566bf6a88b04da30dae31520d3a
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-20 08:25:39 +0800

    Include company and tags in job query

--- app/jobs/queries/getJob.ts
+++ app/jobs/queries/getJob.ts
@@ -1,7 +1,13 @@
-import db, {FindOneJobArgs} from 'db'
+import db, { FindOneJobArgs } from "db"
 
 export default async function getJob(args: FindOneJobArgs) {
-  const job = await db.job.findOne(args)
+  const job = await db.job.findOne({
+    ...args,
+    include: {
+      company: true,
+      tags: true,
+    },
+  })
 
   return job
 }
