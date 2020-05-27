0ae603fd0a5a5f8f154dab18d29d95a0759423f0
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-27 10:03:32 +0800

    Remove additional import for job creation

--- app/jobs/api/createJob.ts
+++ app/jobs/api/createJob.ts
@@ -1,10 +1,10 @@
-import db, { Job } from "db"
+import db from "db"
 
 const createJob = async (args) => {
   if (!args.data) return false
   if (!args.data.company) return false
   // Find job if exists
-  let jobFound: Job
+  let jobFound
   if (args.data.aggId) {
     jobFound = await db.job.findOne({
       where: {
