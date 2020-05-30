a2e27477cf7c6f25e670e871fa5bca0fd9651c93
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-30 20:16:41 +0800

    Create all tags for selection

---
+++ app/components/TagsSelect.tsx
@@ -0,0 +1,45 @@
+import Select from "react-select"
+import makeAnimated from "react-select/animated"
+import { Level } from "react-bulma-components"
+
+const animatedComponents = makeAnimated()
+
+const TagsSelect = (props) => {
+  const { tags, selectedTags, setSelectedTags } = props
+
+  return tags.length ? (
+    <Level>
+      <Level.Side align="left">
+        <p className="heading" style={{ marginRight: 10, alignSelf: "center" }}>
+          Filter by tags:
+        </p>
+      </Level.Side>
+      <Level.Item>
+        <Select
+          closeMenuOnSelect={true}
+          components={animatedComponents}
+          isMulti
+          options={tags.map((tag) => ({ value: tag, label: tag }))}
+          value={selectedTags.map((t) => ({ value: t, label: t }))}
+          onChange={(e) =>
+            setSelectedTags((prevTags) => {
+              if (!e) return []
+              const newTags = e.map((t) => t.value)
+              if (prevTags !== newTags) return newTags
+            })
+          }
+          styles={{
+            container: (p) => ({
+              ...p,
+              width: "100%",
+            }),
+          }}
+        />
+      </Level.Item>
+    </Level>
+  ) : (
+    <></>
+  )
+}
+
+export default TagsSelect
