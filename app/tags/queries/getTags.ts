5452fa6306729eff6780d9f5d41bb719d04810a8
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-30 19:13:33 +0800

    Get tags in asc order

--- app/tags/queries/getTags.ts
+++ app/tags/queries/getTags.ts
@@ -1,7 +1,12 @@
-import db, {FindManyTagArgs} from 'db'
+import db, { FindManyTagArgs } from "db"
 
 export default async function getTags(args: FindManyTagArgs) {
-  const tags = await db.tag.findMany(args)
+  const tags = await db.tag.findMany({
+    orderBy: {
+      name: "asc",
+    },
+    ...args,
+  })
 
   return tags
 }
