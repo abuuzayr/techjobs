3a4360d74b28467bacdd755faefc9fa1dc75db18
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-05 22:54:10 +0800

    Hardcode hero bg

--- app/styles/common.tsx
+++ app/styles/common.tsx
@@ -36,7 +36,7 @@ export const FillCode = styled(AiFillCode)`
 `
 
 export const HeroBody = styled(Hero.Body)`
-  background-image: url(${process.env.BACKGROUND_URL});
+  background-image: url("https://f000.backblazeb2.com/file/js-talks/bg.jpg");
   background-size: cover;
   background-position: center;
   background-color: rgba(0, 0, 0, 0.2);
