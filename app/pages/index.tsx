ccaf3862d1fda045dbe78e8df04dde560bc48ce3
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-06 23:34:30 +0800

    Add SEO meta tags for index

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -54,8 +54,32 @@ const Home = () => {
   return (
     <LikedContext.Provider value={{ liked, setLiked }}>
       <Head>
-        <title>techjobs</title>
+        <title>Find a Tech Job in Singapore | techjobs.sg</title>
         <link rel="icon" href="/favicon.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Search for your dream tech job on techjobs.sg, where our jobs are sourced from the top local job listing platforms in Singapore."
+        ></meta>
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://techjobs.sg/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Find a Tech Job in Singapore | techjobs.sg" />
+        <meta
+          property="og:description"
+          content="Search for your dream tech job on techjobs.sg, where our jobs are sourced from the top local job listing platforms in Singapore."
+        />
+        <meta property="og:image" content="https://techjobs.sg/favicon.png" />
+        <meta property="og:url" content="https://techjobs.sg/" />
+        <meta property="og:site_name" content="techjobs.sg" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="Find a Tech Job in Singapore | techjobs.sg" />
+        <meta
+          property="twitter:description"
+          content="Search for your dream tech job on techjobs.sg, where our jobs are sourced from the top local job listing platforms in Singapore."
+        />
+        <meta property="twitter:image" content="https://techjobs.sg/favicon.png" />
+        <meta property="twitter:site" content="https://techjobs.sg/" />
       </Head>
       <Hero {...{ tab, setTab, search, setSearch, liked, setScrollTo }} />
       {!allowedTabs.includes(tab) && <ContentNotFound />}
