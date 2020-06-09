ea211a6a1a73684f82ffe97a91ad366577a13e64
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-09 12:27:49 +0800

    Add getJob API

---
+++ app/jobs/api/getJob.ts
@@ -0,0 +1,29 @@
+import db, { FindOneJobArgs } from "db"
+
+async function getJob(args: FindOneJobArgs) {
+  const job = await db.job.findOne(args)
+
+  return job
+}
+
+export default async (req, res) => {
+  if (req.method === "GET") {
+    if (!req.query.aggId) {
+      res.statusCode = 400
+      res.end("No id provided")
+    }
+    const job = await getJob({ where: { aggId: req.query.aggId } })
+    if (job) {
+      res.statusCode = 200
+      res.setHeader("Content-Type", "application/json")
+      res.end(JSON.stringify(job))
+    } else {
+      res.statusCode = 404
+      res.end("Job not found")
+    }
+  } else {
+    // Handle any other HTTP method
+    res.setHeader("Allow", ["GET"])
+    res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
