32496f670aeb23cbf95107a7bb00a215cf860688
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-18 12:54:37 +0800

    use blitz userouter instead of next

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { useEffect } from "react"
-import Router from "next/router"
+import { useRouter } from "blitz"
 import * as Fathom from "fathom-client"
 import "react-bulma-components/dist/react-bulma-components.min.css"
 
-// Record a pageview when route changes
-Router.events.on("routeChangeComplete", () => {
-  Fathom.trackPageview()
-})
-
 export default function MyApp({ Component, pageProps }) {
+  const Router = useRouter()
+
+  // Record a pageview when route changes
+  Router.events.on("routeChangeComplete", () => {
+    Fathom.trackPageview()
+  })
+
   // Initialize Fathom when the app loads
   useEffect(() => {
     if (process.env.NODE_ENV === "production") {
