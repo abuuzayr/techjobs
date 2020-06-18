d2243682116c483898a427f7f6f799343e776d82
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-18 13:15:10 +0800

    Wrap router events in useEffect

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,21 +1,19 @@
 import { useEffect } from "react"
-import { useRouter } from "blitz"
+import { Router } from "blitz"
 import * as Fathom from "fathom-client"
 import "react-bulma-components/dist/react-bulma-components.min.css"
 
 export default function MyApp({ Component, pageProps }) {
-  const Router = useRouter()
-
-  // Record a pageview when route changes
-  Router.events.on("routeChangeComplete", () => {
-    Fathom.trackPageview()
-  })
-
   // Initialize Fathom when the app loads
   useEffect(() => {
     if (process.env.NODE_ENV === "production") {
       Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID)
       Fathom.trackPageview()
+
+      // Record a pageview when route changes
+      Router.events.on("routeChangeComplete", () => {
+        Fathom.trackPageview()
+      })
     }
   }, [])
   return <Component {...pageProps} />
