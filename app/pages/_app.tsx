61203757455d5d6d8a411fb8cb960c90384e054d
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-18 10:26:58 +0800

    Use fathom client to programatically update route

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,5 +1,20 @@
-import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { useEffect } from "react"
+import Router from "next/router"
+import * as Fathom from "fathom-client"
+import "react-bulma-components/dist/react-bulma-components.min.css"
+
+// Record a pageview when route changes
+Router.events.on("routeChangeComplete", () => {
+  Fathom.trackPageview()
+})
 
 export default function MyApp({ Component, pageProps }) {
+  // Initialize Fathom when the app loads
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID)
+      Fathom.trackPageview()
+    }
+  }, [])
   return <Component {...pageProps} />
 }
