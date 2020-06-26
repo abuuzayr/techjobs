090fc1fdb25064e5e64e834905a8161e16a545ee
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-27 01:15:40 +0800

    Add subscribe component to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,6 +3,7 @@ import { Container, Level } from "react-bulma-components"
 import styled from "styled-components"
 import { FiGithub, FiSun, FiZap } from "react-icons/fi"
 import { IconContext } from "react-icons"
+import Subscribe from "./Subscribe"
 
 const Foot = styled.footer`
   border-top: 1px solid #000;
@@ -20,33 +21,36 @@ const Foot = styled.footer`
 
 const Footer = () => {
   return (
-    <Foot>
-      <IconContext.Provider value={{ style: { margin: "0 5px", verticalAlign: "middle" } }}>
-        <Container>
-          <Level>
-            <Level.Side align="left" className="level-item">
-              <Link href="/?tab=about" as="/category/about" scroll={false}>
-                <a className="heading">About</a>
-              </Link>
-            </Level.Side>
-            <Level.Item>
-              <p className="">
-                Built in <FiSun /> with{" "}
-                <a href="https://github.com/blitz-js/blitz/" rel="noopener noreferrer" target="_blank">
-                  <FiZap />
-                  Blitz
+    <>
+      <Subscribe />
+      <Foot>
+        <IconContext.Provider value={{ style: { margin: "0 5px", verticalAlign: "middle" } }}>
+          <Container>
+            <Level>
+              <Level.Side align="left" className="level-item">
+                <Link href="/?tab=about" as="/category/about" scroll={false}>
+                  <a className="heading">About</a>
+                </Link>
+              </Level.Side>
+              <Level.Item>
+                <p className="">
+                  Built in <FiSun /> with{" "}
+                  <a href="https://github.com/blitz-js/blitz/" rel="noopener noreferrer" target="_blank">
+                    <FiZap />
+                    Blitz
+                  </a>
+                </p>
+              </Level.Item>
+              <Level.Side align="right" className="level-item">
+                <a href="https://www.github.com/abuuzayr" rel="noopener noreferrer" target="_blank">
+                  <FiGithub />
                 </a>
-              </p>
-            </Level.Item>
-            <Level.Side align="right" className="level-item">
-              <a href="https://www.github.com/abuuzayr" rel="noopener noreferrer" target="_blank">
-                <FiGithub />
-              </a>
-            </Level.Side>
-          </Level>
-        </Container>
-      </IconContext.Provider>
-    </Foot>
+              </Level.Side>
+            </Level>
+          </Container>
+        </IconContext.Provider>
+      </Foot>
+    </>
   )
 }
 
