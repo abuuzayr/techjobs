8b4125d3d98add6869c7778a8c6409c7c3a81faa
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-27 19:27:24 +0800

    Move footer into its own div

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,15 +6,17 @@ import { IconContext } from "react-icons"
 import Subscribe from "./Subscribe"
 
 const Foot = styled.footer`
-  border-top: 1px solid #000;
-  padding: 30px;
-  margin: 50px 50px 0;
-  a {
-    color: black;
-  }
-  @media screen and (max-width: 768px) {
-    .level-item {
-      justify-content: center;
+  .footer {
+    border-top: 1px solid #000;
+    padding: 30px;
+    margin: 50px 50px 0;
+    a {
+      color: black;
+    }
+    @media screen and (max-width: 768px) {
+      .level-item {
+        justify-content: center;
+      }
     }
   }
 `
@@ -23,31 +25,33 @@ const Footer = () => {
   return (
     <Foot>
       <Subscribe />
-      <IconContext.Provider value={{ style: { margin: "0 5px", verticalAlign: "middle" } }}>
-        <Container>
-          <Level>
-            <Level.Side align="left" className="level-item">
-              <Link href="/?tab=about" as="/category/about" scroll={false}>
-                <a className="heading">About</a>
-              </Link>
-            </Level.Side>
-            <Level.Item>
-              <p className="">
-                Built in <FiSun /> with{" "}
-                <a href="https://github.com/blitz-js/blitz/" rel="noopener noreferrer" target="_blank">
-                  <FiZap />
-                  Blitz
+      <div className="footer">
+        <IconContext.Provider value={{ style: { margin: "0 5px", verticalAlign: "middle" } }}>
+          <Container>
+            <Level>
+              <Level.Side align="left" className="level-item">
+                <Link href="/?tab=about" as="/category/about" scroll={false}>
+                  <a className="heading">About</a>
+                </Link>
+              </Level.Side>
+              <Level.Item>
+                <p className="">
+                  Built in <FiSun /> with{" "}
+                  <a href="https://github.com/blitz-js/blitz/" rel="noopener noreferrer" target="_blank">
+                    <FiZap />
+                    Blitz
+                  </a>
+                </p>
+              </Level.Item>
+              <Level.Side align="right" className="level-item">
+                <a href="https://www.github.com/abuuzayr" rel="noopener noreferrer" target="_blank">
+                  <FiGithub />
                 </a>
-              </p>
-            </Level.Item>
-            <Level.Side align="right" className="level-item">
-              <a href="https://www.github.com/abuuzayr" rel="noopener noreferrer" target="_blank">
-                <FiGithub />
-              </a>
-            </Level.Side>
-          </Level>
-        </Container>
-      </IconContext.Provider>
+              </Level.Side>
+            </Level>
+          </Container>
+        </IconContext.Provider>
+      </div>
     </Foot>
   )
 }
