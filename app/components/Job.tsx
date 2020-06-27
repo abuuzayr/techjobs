e50117f9d3f7ecb9f6e7f7b90ff223824b64cb33
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-06-27 23:57:43 +0800

    Alter page title when job is opened

--- app/components/Job.tsx
+++ app/components/Job.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "blitz"
+import { Link, Head } from "blitz"
 import { Box, Media, Content, Container } from "react-bulma-components"
 import Modal from "react-modal"
 import styled from "styled-components"
@@ -40,48 +40,55 @@ const Job = (props) => {
     postedDays = postedAge / 1000 / 60 / 60 / 24
   }
   return (
-    <JobBox old={postedDays > 31 ? 1 : 0} featured={type === "featured"}>
-      <Link href={`/?jobId=${id}`} as={`/jobs/${id}-${slug}`} scroll={false}>
-        <div>
-          <Media renderAs="article" className="is-hidden-mobile">
-            <Media.Item position="left">
-              <Logo job={props.data} />
-            </Media.Item>
-            <Media.Item>
-              <Content>
+    <>
+      <Head>
+        <title>
+          {name} @ {company && company.name} | techjobs.sg
+        </title>
+      </Head>
+      <JobBox old={postedDays > 31 ? 1 : 0} featured={type === "featured"}>
+        <Link href={`/?jobId=${id}`} as={`/jobs/${id}-${slug}`} scroll={false}>
+          <div>
+            <Media renderAs="article" className="is-hidden-mobile">
+              <Media.Item position="left">
+                <Logo job={props.data} />
+              </Media.Item>
+              <Media.Item>
+                <Content>
+                  {type === "featured" && <div className="heading">featured</div>}
+                  <div>
+                    <strong>{name}</strong> @ <small>{company && company.name}</small>
+                  </div>
+                  <JobMeta {...props} postedDays={postedDays} select={true} />
+                </Content>
+              </Media.Item>
+              <Media.Item position="right">
+                <Like id={id} />
+                <Share id={id} slug={slug} />
+                <Container>
+                  <Apply url={url} />
+                </Container>
+              </Media.Item>
+            </Media>
+            <div className="is-hidden-tablet" style={{ textAlign: "center" }}>
+              <Logo job={props.data} style={{ margin: "0 auto" }} />
+              <Content style={{ marginTop: 10 }}>
                 {type === "featured" && <div className="heading">featured</div>}
                 <div>
                   <strong>{name}</strong> @ <small>{company && company.name}</small>
                 </div>
                 <JobMeta {...props} postedDays={postedDays} select={true} />
               </Content>
-            </Media.Item>
-            <Media.Item position="right">
-              <Like id={id} />
-              <Share id={id} slug={slug} />
-              <Container>
+              <MobileActions>
                 <Apply url={url} />
-              </Container>
-            </Media.Item>
-          </Media>
-          <div className="is-hidden-tablet" style={{ textAlign: "center" }}>
-            <Logo job={props.data} style={{ margin: "0 auto" }} />
-            <Content style={{ marginTop: 10 }}>
-              {type === "featured" && <div className="heading">featured</div>}
-              <div>
-                <strong>{name}</strong> @ <small>{company && company.name}</small>
-              </div>
-              <JobMeta {...props} postedDays={postedDays} select={true} />
-            </Content>
-            <MobileActions>
-              <Apply url={url} />
-              <Like id={id} />
-              <Share id={id} slug={slug} />
-            </MobileActions>
+                <Like id={id} />
+                <Share id={id} slug={slug} />
+              </MobileActions>
+            </div>
           </div>
-        </div>
-      </Link>
-    </JobBox>
+        </Link>
+      </JobBox>
+    </>
   )
 }
 
