837531cd38cd0fa98c96b4d1382c22646e3187c3
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-09-30 09:29:25 +0800

    Update unsplash credit to new photog

--- app/components/UnsplashCredit.tsx
+++ app/components/UnsplashCredit.tsx
@@ -5,10 +5,10 @@ const UnsplashCredit = () => (
     <Level.Side align="right">
       <a
         className="unsplash"
-        href="https://unsplash.com/@kp89_?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+        href="https://unsplash.com/@peterng1618?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText"
         target="_blank"
         rel="noopener noreferrer"
-        title="Download free do whatever you want high-resolution photos from Kirill Petropavlov"
+        title="Download free do whatever you want high-resolution photos from Peter Nguyen"
       >
         <span>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
@@ -16,7 +16,7 @@ const UnsplashCredit = () => (
             <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
           </svg>
         </span>
-        <span>Kirill Petropavlov</span>
+        <span>Peter Nguyen</span>
       </a>
     </Level.Side>
     <style jsx>{`
