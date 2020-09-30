e10357152df28210cac86195283b0afb1264874b
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-09-30 09:28:41 +0800

    Update to new bg

--- blitz.config.js
+++ blitz.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   env: {
     NEXT_PUBLIC_FATHOM_SITE_ID: "MBVHBUVD",
-    NEXT_PUBLIC_BACKGROUND_URL: "https://f000.backblazeb2.com/file/js-talks/bg.jpg",
+    NEXT_PUBLIC_BACKGROUND_URL: "https://f000.backblazeb2.com/file/js-talks/bg2.jpg",
     NEXT_PUBLIC_FORM_EMBED_IFRAME:
       '<iframe class="airtable-embed" src="https://airtable.com/embed/shrnVkMKyRztuJ3qq?backgroundColor=cyan" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;height: 2350px;"></iframe>',
   },
