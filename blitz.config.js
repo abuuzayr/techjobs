172ba79a0cdc284b57cb4b081ed415a39e42b6ed
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-10-04 23:07:32 +0800

    Get config from .env file

--- blitz.config.js
+++ blitz.config.js
@@ -11,9 +11,8 @@ module.exports = {
     return config
   },
   env: {
-    NEXT_PUBLIC_FATHOM_SITE_ID: "MBVHBUVD",
-    NEXT_PUBLIC_BACKGROUND_URL: "https://f000.backblazeb2.com/file/js-talks/bg2.jpg",
-    NEXT_PUBLIC_FORM_EMBED_IFRAME:
-      '<iframe class="airtable-embed" src="https://airtable.com/embed/shrnVkMKyRztuJ3qq?backgroundColor=cyan" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;height: 2350px;"></iframe>',
+    NEXT_PUBLIC_FATHOM_SITE_ID: process.env.NEXT_PUBLIC_FATHOM_SITE_ID,
+    NEXT_PUBLIC_BACKGROUND_URL: process.env.NEXT_PUBLIC_BACKGROUND_URL,
+    NEXT_PUBLIC_FORM_EMBED_IFRAME: process.env.NEXT_PUBLIC_FORM_EMBED_IFRAME,
   },
 }
