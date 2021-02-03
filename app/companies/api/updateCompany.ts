89ade8d3e2b9ea56db5c6a5edde2784c82ef7e24
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-02-03 12:56:58 +0800

    Move property deletion outside of API

--- app/companies/api/updateCompany.ts
+++ app/companies/api/updateCompany.ts
@@ -1,10 +1,6 @@
 import db, { CompanyUpdateArgs } from "db"
 
 async function updateCompany(args: CompanyUpdateArgs) {
-  // Don't allow updating unique fields
-  delete args.data.id
-  delete args.data.name
-
   const company = await db.company.update(args)
 
   return company
@@ -17,6 +13,9 @@ export default async (req, res) => {
       res.statusCode = 400
       res.end("No body provided")
     }
+    const data = { ...req.body }
+    delete data.id
+    delete data.name
     const company = await updateCompany({ data: req.body, where: { name: req.body.name } })
     if (company) {
       res.statusCode = 200
