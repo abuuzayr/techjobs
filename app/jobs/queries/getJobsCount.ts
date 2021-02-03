c1657879352f57fe745b0592f9ef04715e4667f2
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-02-03 09:49:09 +0800

    Update types for getJobsCount

--- app/jobs/queries/getJobsCount.ts
+++ app/jobs/queries/getJobsCount.ts
@@ -1,6 +1,8 @@
 import db, { FindManyJobArgs } from "db"
 
-export default async function getJobsCount(args: FindManyJobArgs) {
+type GetJobsInput = Pick<FindManyJobArgs, "where" | "orderBy" | "skip" | "take">
+
+export default async function getJobsCount(args: GetJobsInput) {
   const count = await db.job.count(args)
 
   return count
