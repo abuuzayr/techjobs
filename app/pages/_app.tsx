3157bb47d532c7930b4c068eb0486177df81bebd
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-05-06 15:08:49 +0800

    Load panelbear in site

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,10 +2,13 @@ import { AppProps, ErrorComponent, useRouter } from "blitz"
 import { ErrorBoundary, FallbackProps } from "react-error-boundary"
 import { queryCache } from "react-query"
 import "react-bulma-components/dist/react-bulma-components.min.css"
+import { usePanelbear } from "app/hooks/usePanelbear"
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
   const router = useRouter()
+  // Load Panelbear only once during the app lifecycle
+  usePanelbear(process.env.NEXT_PUBLIC_PANELBEAR_SITE_ID, {})
 
   return (
     <ErrorBoundary
