092be33be319338985310d31db00f89195a4a43c
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-05-11 00:29:30 +0800

    Move constants to export file

---
+++ app/core/constants.ts
@@ -0,0 +1,46 @@
+export const SOURCES = [
+  {
+    aggPrefix: "TIA",
+    name: "Tech in Asia",
+    logoPath: "/TechInAsia-logo.svg",
+  },
+  {
+    aggPrefix: "ADZ",
+    name: "Adzuna",
+    logoPath: "/Adzuna-logo.svg",
+  },
+  {
+    aggPrefix: "SO",
+    name: "Stack Overflow",
+    logoPath: "/StackOverflow-logo.svg",
+  },
+  {
+    aggPrefix: "ADZ",
+    name: "eFinancialCareers",
+    via: "Adzuna",
+    logoPath: "/eFinancialCareers-logo.svg",
+  },
+  {
+    aggPrefix: "ADZ",
+    name: "JobsDB",
+    via: "Adzuna",
+    logoPath: "/jobsDB.png",
+  },
+  {
+    aggPrefix: "ADZ",
+    name: "JobsCentral",
+    via: "Adzuna",
+    logoPath: "/jobsCentral.png",
+  },
+  {
+    aggPrefix: "ADZ",
+    name: "JobStreet",
+    via: "Adzuna",
+    logoPath: "/jobstreet.png",
+  },
+  {
+    aggPrefix: "MCF",
+    name: "MyCareersFuture",
+    logoPath: "/myCareersFuture-logo.png",
+  },
+]
