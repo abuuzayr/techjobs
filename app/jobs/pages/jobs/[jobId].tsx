4e20f92dac080b40bb39b47cba7cd455c8643b9a
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-05-19 17:43:25 +0800

    Prepare job pages for static build

--- app/jobs/pages/jobs/[jobId].tsx
+++ app/jobs/pages/jobs/[jobId].tsx
@@ -1,21 +1,57 @@
-import { useRouter } from "blitz"
-import { Loader, Level } from "react-bulma-components"
+import { useRouter, useParam, BlitzPage, invoke } from "blitz"
+import Layout from "app/core/layouts/Layout"
+import JobContext from "app/components/JobContext"
+import JobContent from "app/components/JobContent"
+import Modal from "react-modal"
+import getJobs from "app/jobs/queries/getJobs"
 
-const JobPage = () => {
+const JobPage: BlitzPage = () => {
+  const id = useParam("jobId", "string")
   const router = useRouter()
-  if (router) {
-    const { pathname, params } = router
-    if (pathname === "/jobs/[jobId]" && params.jobId) {
-      router.push(`/?jobId=${params.jobId}`, `/jobs/${params.jobId}`, { shallow: true })
-    }
-    return (
-      <Level.Item>
-        <Loader style={{ width: 100, height: 100, marginTop: 100 }} />
-      </Level.Item>
-    )
-  }
 
-  return <></>
+  return (
+    <JobContext.Consumer>
+      {({ job, setJob }) => {
+        setJob(id?.split("-")?.[0] || 0)
+        return (
+          <Modal
+            isOpen={id}
+            onRequestClose={() => router.push(router?.query?.referrer as string || "/")}
+            closeTimeoutMS={1000}
+            style={{
+              content: { top: "", right: "", left: "", bottom: "" },
+            }}
+          >
+            {id && <JobContent id={id} router={router} />}
+          </Modal>
+        )
+      }}
+    </JobContext.Consumer>
+  )
 }
 
+JobPage.getLayout = (page) => <Layout title="Job">{page}</Layout>
+
 export default JobPage
+
+export async function getStaticPaths() {
+  const jobs = await invoke(getJobs, {
+    where: {
+      postedDate: {
+        gt: new Date(new Date().getTime() - 3 * 30 * 24 * 60 * 60 * 1000).toISOString(),
+      },
+    },
+    select: { id: true },
+  })
+  return {
+    paths: jobs.map((job) => ({ params: { jobId: `${job.id}` } })),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  return {
+    props: { jobId: params.jobId },
+    revalidate: 1,
+  }
+}
