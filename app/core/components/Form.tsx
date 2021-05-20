e38d287af72b2cd2d0056f4620adb1266786dade
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-05-20 08:25:18 +0800

    Disallow form submission based on stripe states

--- app/core/components/Form.tsx
+++ app/core/components/Form.tsx
@@ -2,7 +2,7 @@ import { ReactNode, PropsWithoutRef } from "react"
 import { Form as FinalForm, FormProps as FinalFormProps } from "react-final-form"
 import * as z from "zod"
 export { FORM_ERROR } from "final-form"
-import { Form as FormComponent, Button } from "react-bulma-components"
+import { Form as BulmaForm, Button } from "react-bulma-components"
 
 export interface FormProps<S extends z.ZodType<any, any>>
   extends Omit<PropsWithoutRef<JSX.IntrinsicElements["form"]>, "onSubmit"> {
@@ -13,6 +13,10 @@ export interface FormProps<S extends z.ZodType<any, any>>
   schema?: S
   onSubmit: FinalFormProps<z.infer<S>>["onSubmit"]
   initialValues?: FinalFormProps<z.infer<S>>["initialValues"]
+  disabled?: boolean
+  processing?: boolean
+  succeeded?: boolean,
+  error?: string
 }
 
 export function Form<S extends z.ZodType<any, any>>({
@@ -21,6 +25,10 @@ export function Form<S extends z.ZodType<any, any>>({
   schema,
   initialValues,
   onSubmit,
+  disabled,
+  processing, 
+  succeeded,
+  error,
   ...props
 }: FormProps<S>) {
   return (
@@ -47,9 +55,9 @@ export function Form<S extends z.ZodType<any, any>>({
           )}
 
           {submitText && (
-            <FormComponent.Control>
-              <Button color="link" disabled={submitting}>{submitText}</Button>
-            </FormComponent.Control>
+            <BulmaForm.Control>
+              <Button color="link" disabled={submitting || disabled || processing || succeeded || !!error}>{succeeded ? "Job created" : submitText}</Button>
+            </BulmaForm.Control>
           )}
         </form>
       )}
